8e331c81503f9ceb057efb7f13f3c410117ba169
Christian Teo <teo.webdev@gmail.com>
2019-11-08 19:04:21 +0200

    FirstMasterCommit

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from '../material/material.module';
+
+
+import { HomePage } from './home.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: HomePage
+      }
+    ]),
+    MaterialModule,
+    ReactiveFormsModule
+  ],
+  declarations: [HomePage]
+})
+export class HomePageModule {}
